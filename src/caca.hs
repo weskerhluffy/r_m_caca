b123644db59113dce045312d070ad3b5ebdebf1e
Darkly Dreaming Wesker <weskerhluffy@gmail.com>
2017-04-20 08:11:27 -0500

    mierda

--- src/caca.hs
+++ src/caca.hs
@@ -1,58 +1,67 @@
-{-# LANGUAGE DatatypeContexts #-}
+-- {-# LANGUAGE DatatypeContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-import Data.IORef
-import System.IO.Unsafe (unsafePerformIO)
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE NamedWildCards #-}
+{-# LANGUAGE TypeOperators #-}
+import Control.Monad
+import Control.Monad.ST
+import Data.STRef
 
-data Arbol a = Nada | Nodo (Arbol a) (Arbol a) (Arbol a) a a
-    deriving Show
+data Arbol a _s = Nada | Nodo (STRef _s (Arbol a _s)) (STRef _s (Arbol a _s)) (STRef _s (Arbol a _s)) a a deriving Show
+-- data Arbol= Nodo{ padre::STRef s Arbol, hi:: STRef Arbol, hdp:: STRef Arbol, val :: Int, idx :: Int} | Nada deriving Show
 
---data Arbol= Nodo{padre :: Arbol, hi:: Arbol, hdp:: IORef Arbol, val :: Int, idx :: Int} | Nada
--- data Arbol= Nodo{ padre::Arbol, hi:: Arbol, hdp:: Arbol, val :: Int, idx :: Int} | Nada
+instance Show (STRef s a) where
+    show _ = "<ioref>"
+--data Arbol= Nodo{padre :: Arbol, hi:: Arbol, hdp:: STRef Arbol, val :: Int, idx :: Int} | Nada
 
-insertar :: (Ord a) => Arbol a -> a -> a -> Arbol a
-insertar Nada x idx = let caca = (Nodo Nada Nada Nada x idx)
-                      in caca
+insertar :: (Ord a) => Arbol a _s-> a -> a -> Arbol a RealWorld#)
+insertar Nada x idx = runST $ do
+    mierda1<-newSTRef Nada
+    mierda2<-newSTRef Nada
+    mierda3<-newSTRef Nada
+    return (Nodo (mierda1) (mierda2) (mierda3) x idx)
 insertar ultimo_insertado@(Nodo padre t1 t2 v idx_padre) x idx = 
     let mierda=insertar_nodo ultimo_insertado Nada x idx
     in mierda
 
-insertar_nodo :: (Ord a) => Arbol a -> Arbol a -> a -> a -> Arbol a
-insertar_nodo Nada anterior@(Nodo padre_ant hi_ant hdp_ant v_ant idx_ant) x idx = do
+insertar_nodo :: (Ord a) => Arbol a s -> Arbol a s-> a -> a -> Arbol a s
+insertar_nodo Nada !anterior@(Nodo padre_ant hi_ant hdp_ant v_ant idx_ant) x idx = do
     let ass = (Nodo Nada anterior Nada x idx)
     ass
-insertar_nodo actual@(Nodo padre t1 t2 v idx_padre) anterior@(Nodo padre_ant hi_ant hdp_ant v_ant idx_ant) x idx
-	| x < v = insertar_nodo padre actual x idx
-	| otherwise  = case t2 of Nada -> let nuevo_nodo= Nodo padre Nada Nada v idx
-	                                      t2_ref = unsafePerformIO(newIORef t2)
-					      caca = writeIORef t2_ref nuevo_nodo
-					  in nuevo_nodo
-				  t2   -> let nodo_nuevo = Nodo padre t2 Nada x idx
-					      (Nodo padre_t2 _ _ _ _ ) = t2
-					      padre_t2_ref = unsafePerformIO(newIORef padre_t2)
-					      caca = writeIORef padre_t2_ref nodo_nuevo
-	                                      t2_ref = unsafePerformIO(newIORef t2)
-					      caca1 = writeIORef t2_ref nodo_nuevo
-					  in nodo_nuevo
+insertar_nodo !actual@(Nodo !padre t1 t2 v idx_padre) !anterior x idx
+    | x < v = insertar_nodo padre actual x idx
+    | otherwise  = case t2 of Nada -> let nuevo_nodo= Nodo actual Nada Nada x idx
+                                          t2_ref = unsafePerformIO(newSTRef t2)
+                                          caca = writeSTRef t2_ref nuevo_nodo
+                                      in nuevo_nodo
+                              t2   -> let nodo_nuevo = Nodo actual t2 Nada x idx
+                                          (Nodo padre_t2 _ _ _ _ ) = t2
+                                          padre_t2_ref = unsafePerformIO(newSTRef padre_t2)
+                                          caca = writeSTRef padre_t2_ref nodo_nuevo
+                                          t2_ref = unsafePerformIO(newSTRef t2)
+                                          caca1 = writeSTRef t2_ref nodo_nuevo
+                                      in nodo_nuevo
 insertar_nodo _ _ _ _ = Nada
 
 --    let ass = (Nodo padre Nada Nada x idx)
---        goodbye = unsafePerformIO(newIORef padre_ant)
---        ad = writeIORef goodbye anterior
+--        goodbye = unsafePerformIO(newSTRef padre_ant)
+--        ad = writeSTRef goodbye anterior
 --    in ass
 
 --insertar_nodo :: Arbol -> Arbol -> Int -> Int -> Arbol 
 --insertar_nodo Nada anterior x idx = do
---    men <- newIORef Nada
+--    men <- newSTRef Nada
 --    return (Nodo men anterior men x idx)
 --insertar_nodo actual@(Nodo padre t1 t2 v idx_padre) anterior@(Nodo padre_ant hi_ant hdp_ant v_ant idx_ant) x idx = do
 --insertar_nodo actual@(Nodo padre t1 t2 v idx_padre) anterior x idx = do
---    men <- newIORef Nada
+--    men <- newSTRef Nada
 --    return (Nodo men men men 0 0)
 
 --
-caca_construye_arbol :: (Ord a, Num a) => [a] -> Arbol a -> a -> Arbol a
+caca_construye_arbol :: (Ord a, Num a) => [a] -> Arbol a s -> a -> Arbol a s
 caca_construye_arbol [] arbolin idx = arbolin
-caca_construye_arbol (x:xs) arbolin idx = caca_construye_arbol xs (insertar arbolin x idx) (idx+1)
+caca_construye_arbol (x:xs) !arbolin idx = caca_construye_arbol xs (insertar arbolin x idx) (idx+1)
 
 --caca_main :: a -> Int
 --caca_main _ = 1
